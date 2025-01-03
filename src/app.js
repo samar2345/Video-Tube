0835f7c6cd2acbc33c3966e19f8e2b1248bdbdfe
Samar Suryawanshi <samar.suryawanshi23@gmail.com>
2025-01-03 13:23:17 +0530

    add refreshAccessToken endpoint

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({exntended:true,limit:"16kb"}))//extended object
 app.use(express.static("public"))//for storing pdfs, images, so that anyone can acces it???
-
+app.use(cookieParser())//used to read and update cookies on the browser of user
 
 //routes import
 import userRouter from './routes/user.routes.js'
@@ -19,5 +19,5 @@ import userRouter from './routes/user.routes.js'
 //routes declaration
 app.use("/api/v1/users",userRouter) //api/v1??
 
-app.use(cookieParser())//used to read and update cookies on the browser of user
+
 export {app}
